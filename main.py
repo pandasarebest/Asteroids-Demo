88f15d8dd1a18d3f86e012237162ebeaca33829c
pandasarebest <s.ed.fox@hotmail.com>
2025-12-10 19:54:34 +0000

    Added movement to character with WASD binds

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 import constants
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,6 +12,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    fps_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -18,7 +22,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = fps_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
